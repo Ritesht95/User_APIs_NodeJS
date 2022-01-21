eba5c4062bd699b6ce8a1392727749f208a43a9f
Ritesh Tailor <rdtailor@gmail.com>
2022-01-21 12:13:06 +0530

    Implements the login and registration APIs

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 class User {
   /**
@@ -8,8 +11,54 @@ class User {
    * @description This method is used to register the user
    */
   userRegistration(req: Request, res: Response) {
-    res.json({ Status: "Success" });
-    // TODO: Implement user registration
+    try {
+      const userData = req.body;
+      const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      const user = UserModel.getUserData(userData.email);
+      if (!userData.fullname || userData.fullname.trim() === "") {
+        res.status(401).send({
+          Status: "Failure",
+          Message: "Please enter full name.",
+        });
+      } else if (!emailRegex.test(userData.email)) {
+        res.status(401).send({
+          Status: "Failure",
+          Message: "Please enter valid email address.",
+        });
+      } else if (userData.password.length < 6) {
+        res.status(401).send({
+          Status: "Failure",
+          Message: "Please enter password with 6 or more characters.",
+        });
+      } else if (user) {
+        res.status(401).send({
+          Status: "Failure",
+          Message: "There is already a user registered with same email.",
+        });
+      } else {
+        const salt = 8;
+        userData.password = bcrypt.hashSync(userData.password, salt);
+        const newUser = new UserModel(
+          userData.fullname,
+          userData.email,
+          userData.password
+        );
+        const result = newUser.save();
+        if (result === true) {
+          res.status(200).send({
+            Status: "Success",
+            Message: "User has been registered successfully.",
+            Data: {},
+          });
+        }
+      }
+    } catch (err) {
+      res.status(500).send({
+        Status: "Failure",
+        Message: "Failed to register the user.",
+        Error: err,
+      });
+    }
   }
 
   /**
@@ -19,8 +68,69 @@ class User {
    * @description This method is used to get user logged in
    */
   userLogin(req: Request, res: Response) {
-    res.json({ Status: "Success" });
-    // TODO: Implement user login
+    try {
+      const email = req.body.Email;
+      const password = req.body.Password;
+      const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      if (!emailRegex.test(email)) {
+        res.status(401).send({
+          Status: "Failure",
+          Message: "Please enter valid email address.",
+        });
+      } else {
+        const user = UserModel.getUserData(email);
+        if (user) {
+          bcrypt.compare(
+            password,
+            user.password,
+            (err: any, compResult: any) => {
+              if (compResult === true) {
+                jwt.sign(
+                  { id: user.userId },
+                  "jwt_secret",
+                  async (err: any, token: any) => {
+                    try {
+                      user.token = token;
+                      const result = UserModel.updateUserData(user);
+                      if (result) {
+                        res.status(200).send({
+                          Status: "Success",
+                          Message: "User logged in succesfully.",
+                          Data: user,
+                        });
+                      }
+                    } catch (error) {
+                      res.status(400).send({
+                        Status: "Failure",
+                        Message: "Failed to login!",
+                        Error: error,
+                      });
+                    }
+                  }
+                );
+              } else {
+                res.status(401).send({
+                  Status: "Failure",
+                  Message: "Incorrect Password!",
+                  Error: err,
+                });
+              }
+            }
+          );
+        } else {
+          res.status(500).send({
+            Status: "Failure",
+            Message: "We couldn't find any user with entered email.",
+          });
+        }
+      }
+    } catch (err: any) {
+      res.status(500).send({
+        Status: "Failure",
+        Message: "Failed to login!",
+        Error: err,
+      });
+    }
   }
 }
 
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,75 @@
-class user {
+const fs = require("fs");
+var path = require("path");
+const rootPath = path.resolve(__dirname);
+const userDataFilePath = `${rootPath}/../../src/data/userData.json`;
+
+class User {
+  userId: number;
   fullname: string;
   email: string;
   password: string;
+  token: string;
 
-  constructor(fullname: string, email: string, password: string) {
+  constructor(
+    fullname: string,
+    email: string,
+    password: string,
+    token: string
+  ) {
+    this.userId = new Date().getTime();
     this.fullname = fullname;
     this.email = email;
     this.password = password;
+    this.token = token;
+  }
+
+  save() {
+    try {
+      const allUsers: any = User.getAllUsersData();
+      allUsers.push(this);
+      fs.writeFileSync(userDataFilePath, JSON.stringify(allUsers));
+      return true;
+    } catch (err) {
+      console.error("Error writing file:", err);
+      throw new Error("Error writing file!");
+    }
+  }
+
+  static getUserData(email: string) {
+    try {
+      const jsonString = fs.readFileSync(userDataFilePath);
+      const parsedJSON = JSON.parse(jsonString);
+      const user = parsedJSON.find((u: User) => u.email === email);
+      return user;
+    } catch (err) {
+      console.error("Error reading file:", err);
+      throw new Error("Error reading file!");
+    }
+  }
+
+  static getAllUsersData() {
+    try {
+      const jsonString = fs.readFileSync(userDataFilePath);
+      const parsedJSON = JSON.parse(jsonString);
+      return parsedJSON;
+    } catch (err) {
+      console.error("Error reading file:", err);
+      throw new Error("Error reading file!");
+    }
+  }
+
+  static updateUserData(user: User): boolean {
+    try {
+      const allUsers = User.getAllUsersData();
+      const userIndex = allUsers.indexOf((u: any) => u.userId === user.userId);
+      allUsers[userIndex] = user;
+      fs.writeFileSync(userDataFilePath, JSON.stringify(allUsers));
+      return true;
+    } catch (err) {
+      console.error("Error writing file:", err);
+      throw new Error("Error writing file!");
+    }
   }
 }
+
+module.exports = User;
